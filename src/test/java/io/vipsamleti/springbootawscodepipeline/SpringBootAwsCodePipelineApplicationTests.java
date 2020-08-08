91e4aa0955d64e1c1fb41915787866c2b68fb681
Vipin Samleti <vipins@aikonlabs.com>
2020-08-08 19:32:36 +0530

    spring boot aws code pipeline demo

---
+++ src/test/java/io/vipsamleti/springbootawscodepipeline/SpringBootAwsCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vipsamleti.springbootawscodepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
